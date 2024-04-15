e89b4cd66a6690bf3aa6df9002df53dc53f47c17
tot882 <132853052+tot882@users.noreply.github.com>
2024-04-15 23:48:21 +0300

    Update utils.py

--- hikka/utils.py
+++ hikka/utils.py
@@ -920,7 +920,7 @@ def get_named_platform() -> str:
     if main.IS_CODESPACES:
         return "🐈‍⬛ Codespaces"
 
-    return f"✌️ lavHost {os.environ['LAVHOST']}" if main.IS_LAVHOST else "ToThosT"
+    return f"🍆 ToThosT {os.environ['LAVHOST']}" if main.IS_LAVHOST else "<emoji document_id=5253874021061118203></emoji>🍆<emoji document_id=5255971528469662704></emoji>🍆<emoji document_id=5255982609485283509>🍆</emoji><emoji document_id=5253803983029422224>🍆</emoji>"
 
 
 def get_platform_emoji() -> str:
