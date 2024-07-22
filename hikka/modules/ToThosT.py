51ef5b11b6c5d0e354547c8409a700384a163ea9
tot882 <132853052+tot882@users.noreply.github.com>
2024-07-22 03:56:07 +0300

    Add files via upload

---
+++ hikka/modules/ToThosT.py
@@ -0,0 +1,36 @@
+import asyncio
+import base64
+import contextlib
+import difflib
+import inspect
+import io
+import logging
+import random
+import re
+import typing
+
+import requests
+import rsa
+from hikkatl.tl.types import Message
+from hikkatl.utils import resolve_inline_message_id
+
+from .. import loader, main, utils
+from ..types import InlineCall, InlineQuery
+from ..version import __version__
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class UnitHeta(loader.Module):
+    """Chat ToThosT"""
+
+    strings = {"name": "ToThosT"}
+
+    async def client_ready(self):
+        await self.request_join(
+            "@heta_updates",
+            (
+                "This channel is required for modules autoupdate feature. You can"
+                " configure it in '.cfg UnitHeta'"
+            ),
+        )
