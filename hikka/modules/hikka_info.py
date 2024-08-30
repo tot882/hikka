87132d40975afb1132b49c152e69f9622a8455f9
Exttasy <94182351+Exttasy1@users.noreply.github.com>
2024-08-30 13:27:28 +0300

    Update hikka_info.py

--- hikka/modules/hikka_info.py
+++ hikka/modules/hikka_info.py
@@ -62,17 +62,7 @@ class HikkaInfoMod(loader.Module):
         platform = utils.get_named_platform()
 
         for emoji, icon in [
-            ("🍊", "<emoji document_id=5449599833973203438>🧡</emoji>"),
-            ("🍇", "<emoji document_id=5449468596952507859>💜</emoji>"),
-            ("❓", "<emoji document_id=5407025283456835913>📱</emoji>"),
-            ("🍀", "<emoji document_id=5395325195542078574>🍀</emoji>"),
-            ("🦾", "<emoji document_id=5386766919154016047>🦾</emoji>"),
-            ("🚂", "<emoji document_id=5359595190807962128>🚂</emoji>"),
-            ("🐳", "<emoji document_id=5431815452437257407>🐳</emoji>"),
-            ("🕶", "<emoji document_id=5407025283456835913>📱</emoji>"),
-            ("🐈‍⬛", "<emoji document_id=6334750507294262724>🐈‍⬛</emoji>"),
-            ("✌️", "<emoji document_id=5469986291380657759>✌️</emoji>"),
-            ("📻", "<emoji document_id=5471952986970267163>💎</emoji>"),
+            ("🍆", "<emoji document_id=5346156903858710186>🍆</emoji>")
         ]:
             platform = platform.replace(emoji, icon)
 
