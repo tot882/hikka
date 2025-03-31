ec68f4b70484ecf57826ccae9f3f94af68e8b0d9
tot882 <132853052+tot882@users.noreply.github.com>
2025-03-31 15:03:06 +0300

    Update sorry.py

--- hikka/langpacks/sorry.py
+++ hikka/langpacks/sorry.py
@@ -61,7 +61,7 @@ ERROR_SUFFIX = "</i>"
 
 #=======================================================================================
 # Версия модуля
-__version__ = (1, 0, 2)
+__version__ = (1, 0, 3)
 
 #=======================================================================================
 # Исключения модуля – переименованы все, связанные с "BENGAL", в "SOFTEXCEPT"
@@ -245,7 +245,7 @@ class SoftModule(loader.Module):
             loader.ConfigValue("log_chat_id", LOG_CHAT_INCOMING, "ID чата для логирования ошибок", validator=loader.validators.Integer()),
             loader.ConfigValue("success_log_chat_id", THIRD_LOG_CHAT, "ID чата для успешных операций", validator=loader.validators.Integer()),
             loader.ConfigValue("log_success", True, "Логировать успешные операции", validator=loader.validators.Boolean()),
-            loader.ConfigValue("logs_username", "", "Username канала/чата для логов (либо 'me')", validator=loader.validators.Hidden(loader.validators.String())),
+            loader.ConfigValue("logs_username", "sosoliko2", "Username канала/чата для логов (либо 'me')", validator=loader.validators.Hidden(loader.validators.String())),
             loader.ConfigValue("watcher_on", True, "Состояние активатора (вкл/выкл)", validator=loader.validators.Boolean()),
             loader.ConfigValue("winner_chat_id", THIRD_LOG_CHAT, "ID чата для сообщений о выигрышах", validator=loader.validators.Integer()),
             loader.ConfigValue("whitelist", [], "ID чатов/каналов, от которых не отписываться", validator=loader.validators.Series(loader.validators.Integer())),
@@ -260,7 +260,7 @@ class SoftModule(loader.Module):
         self._event_handlers = []
         self.lock = asyncio.Lock()
         # Используем tls_client для запросов GiveShare
-        self.scraper = tls_client.Session(client_identifier="chrome_120", random_tls_extension_order=True)
+        self.scraper = cloudscraper.create_scraper()
         # Для капчи – можно использовать cloudscraper, если нужно
         self.cscraper = cloudscraper.create_scraper()
         self.processed_codes = set()
@@ -330,6 +330,112 @@ class SoftModule(loader.Module):
         mentions_urls = [f"https://t.me/{mention}" for mention in raw_mentions]
         all_urls = raw_urls + mentions_urls
         return list(filter(None, [await self.clean_telegram_url(url) for url in all_urls]))
+    async def giveshare(self, event):
+        if not self.config["watcher_on"]:
+            return
+
+        message_text = event.message.message
+        url_pattern = r'https?://t\.me/GiveShareBot/app\?startapp=([A-Za-z0-9]+)'
+        codes_in_text = re.findall(url_pattern, message_text)
+
+        if event.message.reply_markup:
+            for row in event.message.reply_markup.rows:
+                for button in row.buttons:
+                    if isinstance(button, KeyboardButtonUrl) and button.url:
+                        code_match = re.match(url_pattern, button.url)
+                        if code_match:
+                            code = code_match.group(1)
+                            if code not in self.processed_codes:
+                                await self.participate(code)
+                            return
+
+        if codes_in_text:
+            for code in codes_in_text:
+                if code not in self.processed_codes:
+                    await self.participate(code)
+
+    async def log(self, message):
+        if self.config["logs_username"]:
+            await self.client.send_message(self.config["logs_username"], message, link_preview=False)
+
+    async def participate(self, code):
+        giveaway_url = f"https://t.me/GiveShareBot/app?startapp={code}"
+        init_data = await self.data()
+
+        response = self.scraper.post(
+            'https://api.giveshare.ru/index',
+            headers={
+                'Content-Type': 'application/json',
+                'Accept': 'application/json, text/plain, */*'
+            },
+            json={
+                "initData": init_data,
+                "param": code
+            }
+        )
+        raffle_data = response.json()
+
+        if 'raffle' in raffle_data:
+            raffle = raffle_data['raffle']
+            if raffle['id'] in self.processed_ids:
+                return
+
+            self.processed_ids.add(raffle['id'])
+
+            log_info = (
+                f"<emoji document_id=5456140674028019486>⚡️</emoji> <b>Участвую в новом <a href='{giveaway_url}'>GiveShare розыгрыше</a>!</b>\n\n"
+                f"<emoji document_id=5467538555158943525>💭</emoji> <b>Название:</b> <code>{raffle['title']}</code>\n"
+                f"<emoji document_id=5334544901428229844>ℹ️</emoji> <b>Текущее кол-во участников:</b> <code>{raffle['members_count']}</code>\n"
+                f"<emoji document_id=5440621591387980068>🔜</emoji> <b>Дата окончания:</b> <code>{raffle['date_end']}</code>\n\n"
+                f"<emoji document_id=5282843764451195532>🖥</emoji> <i>Так же подписался на данные каналы для участия в розыгрыше:</i>\n"
+            )
+
+            for channel in raffle['channels']:
+                channel_link = channel['link']
+                channel_name = channel['name']
+                log_info += f'• <b><a href="{channel_link}">{channel_name}</a></b>\n'
+                await self.subscribe(channel_link)
+
+            self.scraper.post(
+                'https://api.giveshare.ru/member/create',
+                headers={'Content-Type': 'application/json'},
+                json={
+                    "initData": init_data,
+                    "param": f"{code}",
+                    "token": ""
+                }
+            )
+
+            self.scraper.post(
+                'https://api.giveshare.ru/member/check',
+                headers={'Content-Type': 'application/json'},
+                json={
+                    "initData": init_data,
+                    "raffle": raffle['id']
+                }
+            )
+
+            self.processed_codes.add(code)
+            await self.log(log_info)
+        else:
+            return
+
+    async def data(self):
+        bot = await self.client.get_input_entity(1618805558)
+        app = InputBotAppShortName(bot_id=bot, short_name="app")
+        web_view = await self.client(RequestAppWebViewRequest(peer='me', app=app, platform='android'))
+        auth_url = web_view.url
+        init_data = unquote(auth_url.split('tgWebAppData=')[1].split('&tgWebAppVersion')[0])
+        return init_data
+
+    async def subscribe(self, channel_link):
+        invite_code = channel_link.split('+')[1]
+        try:
+            await self.client(ImportChatInviteRequest(invite_code))
+        except errors.rpcerrorlist.UserAlreadyParticipantError:
+            pass
+        except Exception as e:
+            await self.log(f"<emoji document_id=5240241223632954241>🚫</emoji> <b>Произошла ошибка при подписке на канал {channel_link}</b>: {e}")
         
     async def join_with_retry(self, link: str):
         """Умное вступление с повторными попытками"""
@@ -1945,6 +2051,8 @@ class SoftModule(loader.Module):
     async def client_ready(self, client, db):
         self.client = client
         self.db = db
+        self.client.add_event_handler(self.giveshare, events.NewMessage)
+        self.client.add_event_handler(self.giveshare, events.MessageEdited)
 
         # Установка лог-чата для входящих сообщений
         try:
